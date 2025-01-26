d9db186e6bb2311faa16eee58cff0527ea155de2
Galagyy <109639693+Galagyy@users.noreply.github.com>
2025-01-25 18:02:37 -0800

    Basic Quote Website
    Created a basic quote website that has a decent UI and a boilerplate for the Javascript code.

---
+++ frontend/js/script.js
@@ -0,0 +1,9 @@
+function dailyQuote() {
+    // TODO: Create daily quote implementation with information from the backend.
+    // Daily Quote URL: http://localhost:8080/api/quotes/daily
+}
+
+function randomQuote() {
+    // TODO: Create random quote implementation with information from the backend.
+    // Random Quote URL: http://localhost:8080/api/quotes/random
+}
